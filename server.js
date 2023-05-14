f10f2a8dfb36e111e5f50facc757b9fdbc50f851
kumenger <44558777+kumenger@users.noreply.github.com>
2023-05-14 00:17:49 -0600

    gmail details hideen to env files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const MongoStore =require('connect-mongo')(session)
 const userRouter=require('./routes/userRoutes')
 const PORT = process.env.PORT || 3002
 //const cros = require("cors");
-
+require('dotenv').config()
 const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
